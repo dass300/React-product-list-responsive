1767dea7a3326501399350d4780335c673a8fdff
dass300 <das300@gmail.com>
2022-12-10 13:34:01 +0530

    again

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { BrowserRouter } from 'react-router-dom';
 // import App from './App1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
